d04c7eab0e22dfce772469d8ad4690c310e432e2
Matthew Bush <tucker15@gmail.com>
2023-03-03 14:27:33 -0800

    Matthew's aboutMe

--- Milestones/M1/ThrowawayPrototype/client/src/AboutMe/Matt.js
+++ Milestones/M1/ThrowawayPrototype/client/src/AboutMe/Matt.js
@@ -4,6 +4,12 @@ function Matt() {
   return (
     <div className="App">
       <h1>ABOUT Matt</h1>
+      <div className="info">
+        <div>Full Name : Matthew Bush</div>
+        <div>Role : Backend Lead</div>
+        <div>Main Langauge : Python</div>
+        <div>Hobby : Hiking </div>
+      </div>
     </div>
   );
 }
