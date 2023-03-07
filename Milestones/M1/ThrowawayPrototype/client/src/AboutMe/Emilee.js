326e79b1caee662f88b49d6a1ede7c462d6e288b
emnemgirl22 <91351815+emnemgirl22@users.noreply.github.com>
2023-03-07 15:19:46 -0800

    Update About Me for Emilee

--- Milestones/M1/ThrowawayPrototype/client/src/AboutMe/Emilee.js
+++ Milestones/M1/ThrowawayPrototype/client/src/AboutMe/Emilee.js
@@ -4,6 +4,12 @@ function Emilee() {
   return (
     <div className="App">
       <h1>ABOUT Emilee</h1>
+      <div className="info">
+        <div>Full Name : Emilee Padilla</div>
+        <div>Role : Team Lead</div>
+        <div>Main Langauge : Python</div>
+        <div>Hobby : Hiking and my cats.</div>
+      </div>
     </div>
   );
 }
