f619277ff11d273fe8d18b3f6bb189853d8051ee
Matthew Bush <tucker15@gmail.com>
2023-03-09 11:38:37 -0800

    working flask with react

--- application/flaskapp/website/views.py
+++ application/flaskapp/website/views.py
@@ -9,3 +9,8 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+
+@views.route('/react')
+def react():
+    return render_template('index.html', token="From flask back end")
