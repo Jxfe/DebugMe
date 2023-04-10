3ec465e803c30fa623a661b3ce82711fb8dd2708
Ubuntu <ubuntu@ip-172-31-16-53.us-west-2.compute.internal>
2023-04-10 14:58:55 +0000

    Modified AWS deployment setting files; Updated backend requirements.txt; Modified DB URL

--- Milestones/M2/VerticalPrototype/client/src/utils/url.js
+++ Milestones/M2/VerticalPrototype/client/src/utils/url.js
@@ -2,7 +2,7 @@ const API_URL =
   document.location.hostname === "127.0.0.1" ||
   document.location.hostname === "localhost"
     ? "http://127.0.0.1:5000/"
-    : "http://52.41.50.55:5000/";
+    : "http://52.40.222.68:5000/";
 
 export const SEARCH_URL = `${API_URL}search/`;
 export const POST_URL = `${API_URL}posts/`;
