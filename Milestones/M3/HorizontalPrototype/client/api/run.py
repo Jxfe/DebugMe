48cf7d66df28555f1d2bc58b4b3ff9982d6b5e29
Cris Padilla <crispadilla82@gmail.com>
2023-04-16 12:37:05 -0700

    Modified github workflow to host Horizontal Prototype.

--- Milestones/M3/HorizontalPrototype/client/api/run.py
+++ Milestones/M3/HorizontalPrototype/client/api/run.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
-    return 'Backend Working'
+    return 'Hello There!!'
 
 if __name__ == "__main__":
     app.run()
