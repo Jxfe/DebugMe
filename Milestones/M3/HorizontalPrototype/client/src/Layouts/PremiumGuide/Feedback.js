02a066e16c701b429ecbd00449f1e57c631a3c56
bek <xaetbek@gmail.com>
2023-04-25 00:57:31 -0700

    Added feedback form with star rating

--- Milestones/M3/HorizontalPrototype/client/src/Layouts/PremiumGuide/Feedback.js
+++ Milestones/M3/HorizontalPrototype/client/src/Layouts/PremiumGuide/Feedback.js
@@ -4,14 +4,20 @@ import { Link } from "react-router-dom";
 
 
 function SubmitFeedback() {
-    return(
-      <div class="feedback-container">
-        <div class="main">
-          <h1>There is gonna be a textfield with ratings down below.</h1>
-        </div>
-        <Link to="/feedback" class="feedback-btn">Submit</Link> 
-      </div>
-    );
+  return(
+    <div class="feedback-container">
+      <h2>Please, leave an honest feedback about the premium guide!</h2>
+      <textarea>Write something here ...</textarea>
+      <div class="star-wrapper">
+        <a href="#" class="fas fa-star s1 checked"></a>
+        <a href="#" class="fas fa-star s2"></a>
+        <a href="#" class="fas fa-star s3"></a>
+        <a href="#" class="fas fa-star s4"></a>
+        <a href="#" class="fas fa-star s5"></a>
+      </div> <br/><br/><br/><br/><br/>
+      <Link class="submit">Submit</Link>
+    </div>
+  );
 }
 
 
