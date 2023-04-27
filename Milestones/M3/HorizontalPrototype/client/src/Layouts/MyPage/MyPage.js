1855ae9dfc8a865a1ff001c7e8d1dd3186672fdf
Cris Padilla <crispadilla82@gmail.com>
2023-04-26 17:55:34 -0700

    Added MyEvents link to MyPage.

--- Milestones/M3/HorizontalPrototype/client/src/Layouts/MyPage/MyPage.js
+++ Milestones/M3/HorizontalPrototype/client/src/Layouts/MyPage/MyPage.js
@@ -1,7 +1,15 @@
-import React from "react"; // Needed for AWS since it's using node 16
+import React from "react";
+import { Link } from "react-router-dom"; // Needed for AWS since it's using node 16
 
 function MyPage() {
-  return <div>MyPage</div>;
+  return (
+    <div>
+      <div>
+        <h3>My Page</h3>
+        <Link to="/myevents">View Your Events</Link>
+      </div>
+    </div>
+  );
 }
 
 export default MyPage;
