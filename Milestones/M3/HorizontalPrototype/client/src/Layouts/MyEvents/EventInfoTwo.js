5348f663dd49106bb5d59d9507ab6f35f8f38449
Cris Padilla <crispadilla82@gmail.com>
2023-05-03 21:39:08 -0700

    Added disable functionality to image delete button in CreateEvent.

--- Milestones/M3/HorizontalPrototype/client/src/Layouts/MyEvents/EventInfoTwo.js
+++ Milestones/M3/HorizontalPrototype/client/src/Layouts/MyEvents/EventInfoTwo.js
@@ -5,26 +5,30 @@ import Switch from "../../Components/Switch";
 
 const EventInfoTwo = ({ formData, setFormData }) => {
   const inputRef = useRef();
-  const [file, setFile] = useState(null);
   const [previewImage, setPreviewImage] = useState(null);
+  const [deleteBtnEnabled, setDeleteBtnEnabled] = useState(true);
 
   useEffect(() => {
-    if (!file) return;
+    if (!formData.image) return;
 
     let tmp = [];
-    for (let i = 0; i < file.length; i++) {
-      tmp.push(URL.createObjectURL(file[i]));
+    for (let i = 0; i < formData.image.length; i++) {
+      tmp.push(URL.createObjectURL(formData.image[i]));
     }
     const objectUrls = tmp;
     setPreviewImage(objectUrls);
+    setDeleteBtnEnabled(false);
+    //inputRef.current.value = formData.imageName;
 
     return () => {
       URL.revokeObjectURL(objectUrls);
     };
-  }, [file]);
+  }, [formData.image]);
 
   const deleteImage = () => {
+    setFormData({ ...formData, image: null });
     setPreviewImage(null);
+    setDeleteBtnEnabled(true);
     inputRef.current.value = "";
   };
 
@@ -33,6 +37,7 @@ const EventInfoTwo = ({ formData, setFormData }) => {
       <div className="event-form2">
         <img
           className="form-image"
+          defaultValue={""}
           src={previewImage ? previewImage : ""}
           alt="Event Flyer"
           width="100"
@@ -44,15 +49,23 @@ const EventInfoTwo = ({ formData, setFormData }) => {
             className="form-image-input"
             type="file"
             accept="image/jpg, image/jpeg, image/png"
+            required
             onChange={(event) => {
-              console.log(event.target.files[0]);
-              console.log(event.target.files);
+              console.log(event.target.value);
               if (event.target.files && event.target.files.length > 0) {
-                setFile(event.target.files);
+                setFormData({
+                  ...formData,
+                  image: event.target.files,
+                  imageName: event.target.value
+                });
               }
             }}
           />
-          <Button content="Delete Image" onClickEvent={deleteImage} />
+          <Button
+            content="Delete Image"
+            isDisabled={deleteBtnEnabled}
+            onClickEvent={deleteImage}
+          />
         </div>
       </div>
       <div className="members-only-block">
