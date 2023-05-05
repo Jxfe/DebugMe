2e5a0f0591fe7b90c459baadcab440377c92ceb8
Cris Padilla <crispadilla82@gmail.com>
2023-05-04 22:46:51 -0700

    Copy .env file into repo from home directory in AWS

--- Milestones/M4/BetaPrototype/client/api/debugme_api/events/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import request, flash, jsonify, Blueprint, render_template
-
-events = Blueprint('events', __name__)
-
-@events.route("/api/event", methods=['GET'])
-def get_event():
-    return "Women in CS"
--- Milestones/M4/BetaPrototype/client/src/Layouts/Error/Error.js
+++
@@ -1,7 +0,0 @@
-import React from "react"; // Needed for AWS since it's using node 16
-
-function Error() {
-  return <div>No Page</div>;
-}
-
-export default Error;
