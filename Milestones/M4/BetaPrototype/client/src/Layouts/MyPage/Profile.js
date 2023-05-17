92a980541858406fba89e0416c0e66ef8b25e419
Jeff <majeff126@gmail.com>
2023-05-16 20:03:28 -0700

    Profile displays Name, Email & Plan from /whoami

--- Milestones/M4/BetaPrototype/client/src/Layouts/MyPage/Profile.js
+++ Milestones/M4/BetaPrototype/client/src/Layouts/MyPage/Profile.js
@@ -1,20 +1,59 @@
-import React, { useState } from "react"; // Needed for AWS since it's using node 16
+import React, { useState, useEffect, useContext } from "react";
+import { customAxios } from "../../utils/customAxios";
+import AuthContext from "../../Context/AuthProvider";
 
 function Profile() {
+  const { auth } = useContext(AuthContext);
+  const [profile, setProfile] = useState({
+    name: "",
+    email: "",
+    userRank: "",
+  });
+
+  const fetchProfile = async () => {
+    try {
+      const response = await customAxios.get("/api/whoami", {
+      });
+      const data = response.data;
+      setProfile({
+        name: data.username,
+        email: data.email,
+        userRank: mapRankToRole(data.userRank),
+      });
+    } catch (error) {
+      console.error("Error fetching profile data", error);
+    }
+  };
+
+  const mapRankToRole = (rank) => {
+    const roles = {
+      0: "Basic",
+      1: "Premium",
+      2: "Mentor",
+      3: "Premium_mentor",
+      4: "Admin",
+    };
+    return roles[rank];
+  };
+
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+
   return (
     <div>
       <h1>Profile</h1>
       <div className="mypage-profile">
         <p className="personalinfo-field-head">Name</p>
-        <p>Jose Ortiz</p>
+        <p>{profile.name}</p>
       </div>
       <div>
         <p className="personalinfo-field-head">Email</p>
-        <p>joseo@sfsu.edu</p>
+        <p>{profile.email}</p>
       </div>
       <div>
         <p className="personalinfo-field-head">Subscription Plan</p>
-        <p>Free</p>
+        <p>{profile.userRank}</p>
       </div>
     </div>
   );
