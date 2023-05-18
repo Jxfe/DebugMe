7145b5ef1c69fa5f8774cbfb5b12e6fcb4f69c94
Matthew Bush <tucker15@gmail.com>
2023-05-18 01:56:36 -0700

    Format changes

--- Milestones/M4/BetaPrototype/client/src/Layouts/MyPage/Profile.js
+++ Milestones/M4/BetaPrototype/client/src/Layouts/MyPage/Profile.js
@@ -9,8 +9,11 @@ function Profile() {
     name: "",
     email: "",
     userRank: "",
+    bio: "",
+    image_path: "",
   });
 
+  // Fetches the user's profile data
   const fetchProfile = async () => {
     try {
       const response = await customAxios.get("/api/whoami", {
@@ -20,11 +23,102 @@ function Profile() {
         name: data.username,
         email: data.email,
         userRank: mapRankToRole(data.userRank),
+        bio: data.bio,
+        image_path: data.image_path
       });
     } catch (error) {
       console.error("Error fetching profile data", error);
     }
   };
+
+  // Maps user rank to role
+  const mapRankToRole = (rank) => {
+    const roles = {
+      0: "Basic",
+      1: "Premium",
+      2: "Mentor",
+      3: "Premium_mentor",
+      4: "Admin",
+    };
+    return roles[rank];
+  };
+    
+    
+    // State for edited profile data
+    const [editedProfile, setEditedProfile] = useState({
+      newName: "",
+      newBio: "",
+      newImagePath: ""
+    });
+
+    // Handles name edit
+    const handleNameEdit = async () => {
+      try {
+          const response = await customAxios({
+              method: "post",
+              url: "/api/editProfileName",
+              data: {
+                newName: editedProfile.newName,
+                
+              },
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+              }
+            })
+        const data = response.data;
+          
+      } catch (error) {
+        
+        console.error("Error editing profile name", error);
+      }
+    };
+
+    // Handles bio edit
+    const handleBioEdit = async () => {
+      try {
+          const response = await customAxios({
+              method: "post",
+              url: "/api/editProfileBio",
+              data: {
+                //newName: editedProfile.newName,
+                newBio: editedProfile.newBio
+              },
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+              }
+            })
+        const data = response.data;
+        
+        
+      } catch (error) {
+        
+        console.error("Error editing profile name", error);
+      }
+    };
+    
+    // Handles image edit
+    const handleImageEdit = async () => {
+      try {
+          const response = await customAxios({
+              method: "post",
+              url: "/api/editProfileImage",
+              data: {
+                newImagePath: editedProfile.newImagePath
+              },
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+              }
+            })
+        const data = response.data;
+        
+        
+      } catch (error) {
+        
+        console.error("Error editing profile name", error);
+      }
+    };
+    
+
 
   const becomePremium = async () => {
     try {
@@ -64,16 +158,7 @@ function Profile() {
     }
   };
   
-  const mapRankToRole = (rank) => {
-    const roles = {
-      0: "Basic",
-      1: "Premium",
-      2: "Mentor",
-      3: "Premium_mentor",
-      4: "Admin",
-    };
-    return roles[rank];
-  };
+  
 
   useEffect(() => {
     fetchProfile();
@@ -81,27 +166,85 @@ function Profile() {
 
   return (
     <div>
-      <h1>Profile</h1>
-      <div className="mypage-profile">
-        <p className="personalinfo-field-head">Name</p>
-        <div style={{ display: "flex", gap: "40px" }}>
+          <div className="mypage-profile" style={{ display: "flex", gap: "40px"}}>
+          <h1 >Profile</h1>
+      
+            <div style={{ display: "flex", gap: "40px", alignItems: "center" }}>
+              <img
+                src={profile.image_path}
+                alt="User"
+                className="user-image"
+              />
+            </div>
+        </div>
+      <div>
+              <input
+                style={{ width: "150px" }}
+                type="text"
+                value={editedProfile.newImagePath}
+                onChange={(e) =>
+                  setEditedProfile({ ...editedProfile, newImagePath: e.target.value })
+                }
+              />
+              <Button
+                className="default-button"
+                content="Update Image"
+                onClick={handleImageEdit}
+              />
+        </div>
+
+      <div className="mypage-profile" style={{ display: "flex",alignItems: "left", gap: "20px", }}>
+          
+        <p className="personalinfo-field-head">Name:</p>
+        <div style={{ display: "flex", gap: "40px", alignItems: "left", marginTop: "15px" }}>
           <p>{profile.name}</p>
-          <Button
+             
+
+          
+          <input
+            style={{ width: "200px", hight: "50px" }}
+            type="text"
+            value={editedProfile.newName}
+            onChange={(e) =>
+              setEditedProfile({ ...editedProfile, newName: e.target.value })
+            }
+          />
+            <Button
             className="default-button"
             content="Edit"
+            onClick={handleNameEdit}
           />
+            
         </div>
+
+        
+
+        
       </div>
-      <div className="mypage-profile">
-        <p className="personalinfo-field-head">Bio</p>
-        <div style={{ display: "flex", gap: "40px" }}>
-          <p>{profile.name}</p>
-          <Button
+      <div className="mypage-profile" style={{ display: "flex",alignItems: "left", gap: "20px", }}>
+        <p className="personalinfo-field-head">Bio:</p>
+        <div style={{ display: "flex", gap: "40px", alignItems: "left", marginTop: "15px" }}>
+          <p>{profile.bio}</p>
+        </div>
+
+        
+      </div>
+        <div >
+          <input
+            style={{ width: "400px",  }}
+            type="text"
+            value={editedProfile.newBio}
+            onChange={(e) =>
+              setEditedProfile({ ...editedProfile, newBio: e.target.value })
+            }
+          />
+        <Button
             className="default-button"
             content="Edit"
+            onClick={handleBioEdit}
           />
+          
         </div>
-      </div>
       <div>
         <p className="personalinfo-field-head">Email</p>
         <p>{profile.email}</p>
