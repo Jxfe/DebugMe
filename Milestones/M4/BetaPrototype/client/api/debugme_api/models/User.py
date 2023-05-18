40b9615fd87e1844498c92aee9bbbf2118cee86d
Jeff <majeff126@gmail.com>
2023-05-17 17:07:48 -0700

    Unique Username

--- Milestones/M4/BetaPrototype/client/api/debugme_api/models/User.py
+++ Milestones/M4/BetaPrototype/client/api/debugme_api/models/User.py
@@ -7,7 +7,7 @@ from debugme_api.debugme_toolkit import db, ma
 class User(db.Model):
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False, unique =True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     userRank = db.Column(db.Integer, nullable=True, default=0)
